3c3bd556543142c4794ee744ded5ee69dc01e5e6
slaxeea <kappelers77@gmail.com>
2020-09-15 11:41:13 +0200

    tag 3

--- client/onclickfunc.js
+++ client/onclickfunc.js
@@ -1,42 +1,42 @@
-var https = require('https');
+import { https } from 'https';
 
 // Send link
 var num = sessionStorage.getItem("number");
 var imgurl = sessionStorage.getItem(`image${num}`);
 var data = {
-    url: imgurl
+  url: imgurl,
 };
 
 var options = {
-    hostname: 'wouldyourather-a4ad8.firebaseapp.com',
-    path: '/images',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-    }
-}
+  hostname: "wouldyourather-a4ad8.firebaseapp.com",
+  path: "/images",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length,
+  },
+};
 
-const req = https.request(options, (res) => {
-    console.log('Status Code:', res.statusCode);
+const req = https
+  .request(options, (res) => {
+    console.log("Status Code:", res.statusCode);
 
-    res.on('data', (chunk) => {
-        data += chunk;
+    res.on("data", (chunk) => {
+      data += chunk;
     });
 
-    res.on('end', () => {
-        console.log('Body: ', JSON.parse(data));
+    res.on("end", () => {
+      console.log("Body: ", JSON.parse(data));
     });
-
-}).on("error", (err) => {
+  })
+  .on("error", (err) => {
     console.log("Error: ", err.message);
-});
+  });
 
 req.write(data);
 req.end();
 
-
 // Load new Image
-var script = document.createElement('script');  
-script.src = "images.js"; 
+var script = document.createElement("script");
+script.src = "images.js";
 document.head.appendChild(script);
